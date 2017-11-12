93be2de2269a86739ce3da872dc83ddbe323378a
Gustavo Viegas <webviegas@gmail.com>
2017-11-12 14:41:36 -0200

    Initial Commit

---
+++ tp2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tp2
+//
+//  Created by Viegas on 12/11/17.
+//  Copyright © 2017 Ciencia da Computacao - UFV. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
