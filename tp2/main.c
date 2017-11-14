14309233fac3549ca680ed69630ebc9c8fb5644c
Gustavo Viegas <webviegas@gmail.com>
2017-11-14 00:41:16 -0200

    Finite!

--- tp2/main.c
+++ tp2/main.c
@@ -17,79 +17,86 @@
 #define BUFFERSIZE 255
 #define FILENAMESIZE 200
 
-//int permutacao() {
-//    /* Nosso número na base n. Ele é um vetor
-//     * de n+1 posições representando um número
-//     * na base n.
-//     */
-//    int *num ;
-//    /* input é a string de entrada, e str
-//     * receberá cada permutação.
-//     */
-//    char input[MAX], str[MAX] ;
-//    int n, r, i, j, k ;
-//    
-//    printf("Entre com o conjunto inicial: ") ;
-//    scanf("%s", input) ;
-//    
-//    printf("Entre com o r: ") ;
-//    scanf("%d", &r) ;
-//    
-//    /* Aqui elimina-se caracteres repetidos na entrada.
-//     * Esse procedimento não faz parte do algoritmo, e
-//     * só é feito por questões práticas.
-//     */
-//    n = strlen(input) ;
-//    j = 0;
-//    str[0] = 0 ;
-//    for ( i = 0; i < n; i++ ) {
-//        if ( strchr(str, input[i]) == NULL ) {
-//            str[j] = input[i] ;
-//            j++ ;
-//            str[j] = 0 ;
-//        }
-//    }
-//    strcpy(input, str) ;
-//    n = strlen(input) ;
-//    
-//    /* Cria o nosso número. Ele é um vetor de
-//     * r+1 posições, sendo que a última é
-//     * reservada para indicar quando todos os
-//     * números de tamanho r foram gerados. */
-//    num = (int *) calloc( (r+1), sizeof(int)) ;
-//    if ( num == NULL ) {
-//        perror("calloc") ;
-//        return -1;
-//    }
-//    
-//    /* Termina quando a última posição do vetor
-//     * for 1. */
-//    while ( num[r] == 0 ) {
-//        for ( i = 0; i < n; i++ ) {
-//            /* processo de mapeamento. */
-//            for ( j = 0, k = r-1; j < r; j++ ) {
-//                str[k] = input[num[j]] ;
-//                k-- ;
-//            }
-//            /* Mostra o resultado. */
-//            str[r] = 0 ;
-//            printf("%s\n", str) ;
-//            
-//            /* incrementa o algarismo menos significativo. */
-//            num[0]++ ;
-//        }
-//        
-//        /* Muda de "casa" e lança os vai uns. */
-//        for ( i = 0; i < r; i++ ) {
-//            if ( num[i] == n ) {
-//                num[i] = 0 ;
-//                num[i+1]++ ;
-//            }
-//        }
-//    }
-//    
-//    return 0 ;
-//}
+
+int verificaAdjacencia (matrizPBN* M, int r, char* str, int* ehSolucao) {
+    int i, j, invalido = 0;
+    
+    for (i = 0; (i < r && invalido != 1); i++) {
+        for (j = 0; (j < r && invalido != 1); j++) {
+            if (M->valores[i][j] == 1 && str[i] == str[j]) {
+                invalido = 1;
+                break;
+            }
+        }
+    }
+    
+    if (!invalido) {
+        printf("\t **** O conjunto %s é solucao! **** \n", str);
+        *ehSolucao = 1;
+    }
+    
+    return 0;
+}
+
+int permutacao(matrizPBN* M, int r) {
+    /* Nosso número na base n. Ele é um vetor
+     * de n+1 posições representando um número
+     * na base n.
+     */
+    int *num;
+    int ehSolucao = 0;
+
+    char str[MAX];
+    int n, i, j, k;
+    
+    char input[2] = {'A', 'V'};
+    
+    /* Aqui elimina-se caracteres repetidos na entrada.
+     * Esse procedimento não faz parte do algoritmo, e
+     * só é feito por questões práticas.
+     */
+    n = K;
+    
+    /* Cria o nosso número. Ele é um vetor de
+     * r+1 posições, sendo que a última é
+     * reservada para indicar quando todos os
+     * números de tamanho r foram gerados. */
+    num = (int *) calloc( (r+1), sizeof(int)) ;
+    if ( num == NULL ) {
+        perror("calloc") ;
+        return -1;
+    }
+    
+    /* Termina quando a última posição do vetor
+     * for 1. */
+    while ( num[r] == 0 ) {
+        for ( i = 0; i < n; i++ ) {
+            /* processo de mapeamento. */
+            for ( j = 0, k = r-1; j < r; j++ ) {
+                str[k] = input[num[j]] ;
+                k-- ;
+            }
+            /* Mostra o resultado. */
+            str[r] = 0 ;
+            verificaAdjacencia(M, r, str, &ehSolucao);
+            if (ehSolucao == 1) return 1;
+            
+            /* incrementa o algarismo menos significativo. */
+            num[0]++ ;
+        }
+        
+        /* Muda de "casa" e lança os vai uns. */
+        for ( i = 0; i < r; i++ ) {
+            if ( num[i] == n ) {
+                num[i] = 0 ;
+                num[i+1]++ ;
+            }
+        }
+    }
+    
+    return 0 ;
+}
+
 
 int configuraMatriz(int* n, matrizPBN* M) {
     char* caminhoArquivo = "/Users/viegas/Desktop/UFV/AED I/Trabalhos/tp2/tp2/n7.txt";
@@ -138,6 +145,8 @@ int main(int argc, const char * argv[]) {
     
     configuraMatriz(&n, &M);
     printaMatriz(&M, n);
+    
+    permutacao(&M, n);
     
     return 0;
 }
